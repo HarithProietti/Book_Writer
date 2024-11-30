430ffe79a7f7905cb7b1c7c514aa2ec2224812e7
Harith Proietti <harith.proietti@gmail.com>
2024-11-30 23:12:21 +0100

    code for writing all paragraphs working; book written and saved (need to debug chapters' titles and intro)

--- src/app.py
+++ src/app.py
@@ -4,6 +4,8 @@ load_dotenv()
 from characters import MainCharacterChain
 from structure import get_structure
 from events import ChapterPlotChain, EventsChain, get_events
+from writing import write_book
+from book_saving import DocWriter
 
 subject = 'War between the forest and the savanna'
 author = 'William Shakespeare'
@@ -11,9 +13,13 @@ genre = 'Sci-Fi'
 
 main_character_chain = MainCharacterChain()
 profile = main_character_chain.run('Kevin_Richardson_(zookeeper).pdf')
+doc_writer = DocWriter()
 
 title, plot, chapter_dict = get_structure(subject, genre, author, profile)
 summaries_dict, event_dict = get_events(subject, genre, author, profile, title, plot, chapter_dict)
+book = write_book(genre, author, title, profile, plot, summaries_dict, event_dict)
+
+doc_writer.write_doc(book, chapter_dict, title)
 
 # print(title)
 # print(plot)
@@ -23,7 +29,7 @@ summaries_dict, event_dict = get_events(subject, genre, author, profile, title, 
 #     print(chapter)
 #     print(plot)
 #     print()
-for chapter, events in event_dict.items():
-    print(chapter)
-    print(events)
-    print()
+# for chapter, events in event_dict.items():
+#     print(chapter)
+#     print(events)
+#     print()
